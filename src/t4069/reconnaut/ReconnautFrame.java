76c012a02ae0d4b9d75b5ca5096929ecd5d6f37e
Edmund Noble <edmundnoble@gmail.com>
2012-10-03 15:59:46 -0400

    not much change

--- src/t4069/reconnaut/ReconnautFrame.java
+++ src/t4069/reconnaut/ReconnautFrame.java
@@ -2,6 +2,9 @@ package t4069.reconnaut;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,9 +12,12 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class ReconnautFrame extends JFrame {
 
+	private final HashMap<String, HashMap<String, HashMap<String,  HashMap<String, Object>>>> entryMap = 
+			new HashMap<String, HashMap<String, HashMap<String,  HashMap<String, Object>>>>();
 	private static final long serialVersionUID = -4388059251901151481L;
 	private JPanel contentPane;
 
@@ -34,7 +40,7 @@ public class ReconnautFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public ReconnautFrame() {
+	public ReconnautFrame() { // form generation
 		setResizable(false);
 		setTitle("Reconnaut");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,17 +61,32 @@ public class ReconnautFrame extends JFrame {
 		JLabel lblMatchTime = new JLabel("Match Time:");
 		lblMatchTime.setBounds(440, 12, 86, 15);
 		contentPane.add(lblMatchTime);
+		ActionListener comboBoxListener = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JComboBox box = (JComboBox) e.getSource();
+				String str = ((JTextField)box.getEditor().getEditorComponent()).getText().toString()s;
+			}};
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(355, 7, 67, 24);
+		comboBox.setEditable(true);
+		comboBox.setBounds(147, 7, 86, 24);
 		contentPane.add(comboBox);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(156, 7, 77, 24);
+		comboBox_1.setEditable(true);
+		comboBox_1.setBounds(345, 7, 77, 24);
 		contentPane.add(comboBox_1);
 		
 		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.addActionListener(comboBoxListener);
+		comboBox_2.setEditable(true);
 		comboBox_2.setBounds(544, 7, 67, 24);
 		contentPane.add(comboBox_2);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(147, 325, 117, 25);
+		contentPane.add(btnSave);
 	}
 }
