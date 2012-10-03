16ead2525bd7fca247538a1caa5679dd4d9bc2bc
Edmund Noble <edmundnoble@gmail.com>
2012-10-03 18:56:20 -0400

    Added some things.

--- src/t4069/reconnaut/ReconnautFrame.java
+++ src/t4069/reconnaut/ReconnautFrame.java
@@ -16,8 +16,7 @@ import javax.swing.JButton;
 
 public class ReconnautFrame extends JFrame {
 
-	private final HashMap<String, HashMap<String, HashMap<String,  HashMap<String, Object>>>> entryMap = 
-			new HashMap<String, HashMap<String, HashMap<String,  HashMap<String, Object>>>>();
+	private final HashMap<String, HashMap<String, HashMap<String, HashMap<String, Object>>>> entryMap = new HashMap<String, HashMap<String, HashMap<String, HashMap<String, Object>>>>();
 	private static final long serialVersionUID = -4388059251901151481L;
 	private JPanel contentPane;
 
@@ -49,44 +48,51 @@ public class ReconnautFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblTeamNumber = new JLabel("Team Number:");
 		lblTeamNumber.setBounds(31, 12, 107, 15);
 		contentPane.add(lblTeamNumber);
-		
+
 		JLabel lblMatchDate = new JLabel("Match Date:");
 		lblMatchDate.setBounds(251, 12, 86, 15);
 		contentPane.add(lblMatchDate);
-		
+
 		JLabel lblMatchTime = new JLabel("Match Time:");
 		lblMatchTime.setBounds(440, 12, 86, 15);
 		contentPane.add(lblMatchTime);
-		ActionListener comboBoxListener = new ActionListener() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JComboBox box = (JComboBox) e.getSource();
-				String str = ((JTextField)box.getEditor().getEditorComponent()).getText().toString()s;
-			}};
-		
-		JComboBox comboBox = new JComboBox();
+		final JComboBox comboBox = new JComboBox();
 		comboBox.setEditable(true);
 		comboBox.setBounds(147, 7, 86, 24);
 		contentPane.add(comboBox);
-		
-		JComboBox comboBox_1 = new JComboBox();
+
+		final JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setEditable(true);
 		comboBox_1.setBounds(345, 7, 77, 24);
 		contentPane.add(comboBox_1);
-		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.addActionListener(comboBoxListener);
+
+		final JComboBox comboBox_2 = new JComboBox();
 		comboBox_2.setEditable(true);
 		comboBox_2.setBounds(544, 7, 67, 24);
 		contentPane.add(comboBox_2);
-		
+
 		JButton btnSave = new JButton("Save");
 		btnSave.setBounds(147, 325, 117, 25);
+		btnSave.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String number = comboBox.getSelectedItem().toString();
+				if (!entryMap.containsKey(number)) {
+					entryMap.put(
+							number,
+							new HashMap<String, HashMap<String, HashMap<String, Object>>>());
+				}
+				String date = comboBox_1.getSelectedItem().toString();
+				if (!entryMap.get(number).containsKey(date))
+					entryMap.get(number).put(date,
+							new HashMap<String, HashMap<String, Object>>());
+			}
+		});
 		contentPane.add(btnSave);
 	}
 }
