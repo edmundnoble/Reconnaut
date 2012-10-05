996bbc038ee28d3cb68c8909f600890072f20e78
Edmund Noble <edmundnoble@gmail.com>
2012-10-04 23:40:37 -0400

    things with the comboboxes

--- src/t4069/reconnaut/ReconnautFrame.java
+++ src/t4069/reconnaut/ReconnautFrame.java
@@ -1,30 +1,32 @@
+
 package t4069.reconnaut;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.HashMap;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
 
 public class ReconnautFrame extends JFrame {
 
-	private final HashMap<String, HashMap<String, HashMap<String, HashMap<String, Object>>>> entryMap = new HashMap<String, HashMap<String, HashMap<String, HashMap<String, Object>>>>();
+	private final HashMap<String, HashMap<String, HashMap<String, HashMap<String, Object>>>> entryMap =
+			new HashMap<String, HashMap<String, HashMap<String, HashMap<String, Object>>>>();
 	private static final long serialVersionUID = -4388059251901151481L;
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+
 			public void run() {
 				try {
 					ReconnautFrame frame = new ReconnautFrame();
@@ -35,6 +37,11 @@ public class ReconnautFrame extends JFrame {
 			}
 		});
 	}
+
+	private JPanel contentPane;
+	private JComboBox dateBox;
+	private JComboBox numberBox;
+	private JComboBox timeBox;
 
 	/**
 	 * Create the frame.
@@ -54,45 +61,95 @@ public class ReconnautFrame extends JFrame {
 		contentPane.add(lblTeamNumber);
 
 		JLabel lblMatchDate = new JLabel("Match Date:");
-		lblMatchDate.setBounds(251, 12, 86, 15);
+		lblMatchDate.setBounds(222, 12, 86, 15);
 		contentPane.add(lblMatchDate);
 
 		JLabel lblMatchTime = new JLabel("Match Time:");
 		lblMatchTime.setBounds(440, 12, 86, 15);
 		contentPane.add(lblMatchTime);
 
-		final JComboBox comboBox = new JComboBox();
-		comboBox.setEditable(true);
-		comboBox.setBounds(147, 7, 86, 24);
-		contentPane.add(comboBox);
+		numberBox = new JComboBox();
+		numberBox.setEditable(true);
+		numberBox.setBounds(126, 7, 86, 24);
+		contentPane.add(numberBox);
 
-		final JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setEditable(true);
-		comboBox_1.setBounds(345, 7, 77, 24);
-		contentPane.add(comboBox_1);
+		dateBox = new JComboBox();
+		dateBox.setEditable(true);
+		dateBox.setBounds(303, 7, 107, 24);
+		contentPane.add(dateBox);
 
-		final JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setEditable(true);
-		comboBox_2.setBounds(544, 7, 67, 24);
-		contentPane.add(comboBox_2);
+		timeBox = new JComboBox();
+		timeBox.setEditable(true);
+		timeBox.setBounds(519, 7, 86, 24);
+		contentPane.add(timeBox);
+		ItemListener dateListener = new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent arg0) {
+				numberBox.setSelectedItem(arg0.getItem());
+				if (entryMap.get(arg0.getItem().toString()) != null) {
+					dateBox.removeAllItems();
+					for (String s : entryMap
+							.get(arg0.getItem().toString()).keySet()) {
+						dateBox.addItem(s);
+					}
+				}
+			}
+		};
+		numberBox.addItemListener(dateListener);
 
 		JButton btnSave = new JButton("Save");
-		btnSave.setBounds(147, 325, 117, 25);
+		btnSave.setBounds(190, 329, 117, 23);
 		btnSave.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String number = comboBox.getSelectedItem().toString();
-				if (!entryMap.containsKey(number)) {
-					entryMap.put(
-							number,
-							new HashMap<String, HashMap<String, HashMap<String, Object>>>());
-				}
-				String date = comboBox_1.getSelectedItem().toString();
-				if (!entryMap.get(number).containsKey(date))
-					entryMap.get(number).put(date,
-							new HashMap<String, HashMap<String, Object>>());
+				save();
 			}
 		});
 		contentPane.add(btnSave);
+
+		JButton btnNewButton = new JButton("Load");
+		btnNewButton.setBounds(318, 329, 126, 23);
+		contentPane.add(btnNewButton);
+
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(268, 364, 89, 23);
+		contentPane.add(btnExit);
+		btnExit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+	}
+
+	private void refreshBoxes() {
+
+	}
+
+	private void save() {
+		String number = numberBox.getSelectedItem().toString().trim();
+		if (entryMap.get(number) == null) {
+			entryMap.put(
+					number,
+					new HashMap<String, HashMap<String, HashMap<String, Object>>>());
+			numberBox.addItem(number);
+		}
+		String date = dateBox.getSelectedItem().toString().trim();
+		if (entryMap.get(number).get(date) == null) {
+			entryMap.get(number).put(date,
+					new HashMap<String, HashMap<String, Object>>());
+			dateBox.addItem(date);
+		}
+		String time = timeBox.getSelectedItem().toString().trim();
+		if (entryMap.get(number).get(date).get(time) == null) {
+			entryMap.get(number).get(date)
+					.put(time, new HashMap<String, Object>());
+			timeBox.addItem(time);
+		}
+		refreshBoxes();
+
 	}
 }
